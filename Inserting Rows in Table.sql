ca93831b0e252d005f86192609865d78cbecef0e
Yash Mishra <myash0370@gmail.com>
2026-08-05 19:18:47 +0530

    Create Inserting Rows in Table.sql

---
+++ Inserting Rows in Table.sql
@@ -0,0 +1,6 @@
+USE ecom;
+SELECT * FROM clients;
+-- INSERT INTO clients (name, email, age, phone, is_active, signup_date, created_at, total_spent)
+-- VALUES
+-- -- ('Yash Mishra', 'yash@gmail.com', 21, '7060829568', TRUE, 	'2026-06-19', '2026-06-19 4:53:00', 12000.50),
+-- -- ('Rahul Khan', 'rahul@gmail.com', 32, '9988776655', FALSE, '2025-01-15', '2025-01-15 14:20:00', 0.00);
