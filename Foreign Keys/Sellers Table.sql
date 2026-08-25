ecf2c684a2ef16541e7e4416a73c03bad28745a1
Yash Mishra <myash0370@gmail.com>
2026-08-25 23:00:49 +0530

    Create Sellers Table.sql

---
+++ Foreign Keys/Sellers Table.sql
@@ -0,0 +1,10 @@
+-- Step 1: Create sellers Table 
+-- CREATE TABLE sellers(
+-- seller_id INT PRIMARY KEY AUTO_INCREMENT ,
+-- seller_name VARCHAR(100) UNIQUE NOT NULL,
+-- city VARCHAR(50)
+-- );
+
+-- Valid Data Insert
+INSERT INTO sellers (seller_name, city)
+VALUES ('Hp', 'Merrut');
