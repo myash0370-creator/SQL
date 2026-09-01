13ee8b8be11cd976d3571c7040901bd5775f9069
Yash Mishra <myash0370@gmail.com>
2026-09-02 00:18:00 +0530

    Create Sellers information.sql

---
+++ JOINS/Sellers information.sql
@@ -0,0 +1,17 @@
+use ecom;
+-- CREATE TABLE sellers(
+-- seller_id INT AUTO_INCREMENT PRIMARY KEY,
+-- seller_name VARCHAR(100) UNIQUE NOT NULL,
+-- city VARCHAR(50)
+-- );
+-- INSERT INTO sellers(seller_name, city)
+-- VALUES ('HDTECH', 'Banglore');
+-- INSERT INTO sellers(seller_name, city)
+-- VALUES ('TECHNO', 'Delhi')
+-- INSERT INTO sellers(seller_name, city)
+-- VALUES ('GARWAL ELECTRONICS', 'Delhi')
+-- INSERT INTO sellers(seller_name, city)
+-- VALUES ('All In One', 'Kolkata')
+-- INSERT INTO sellers(seller_name, city)
+-- VALUES ('INNOVA', 'Mumbai')
+SELECT * FROM sellers;
