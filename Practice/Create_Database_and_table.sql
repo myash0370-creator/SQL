c357eb139073fae4e3d969974a8f4c69f5e617d7
Yash Mishra <myash0370@gmail.com>
2026-09-10 21:42:18 +0530

    Create Create_Database_and_table.sql

---
+++ Practice/Create_Database_and_table.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE practice;
+USE practice;
+CREATE TABLE student(
+id INT,
+name VARCHAR(30),
+age INT,
+city VARCHAR(50),
+marks INT);
+INSERT INTO student VALUES
+(1,'Yash',21,'Rampur',85),
+(2,'Aman',20,'Delhi',78),
+(3,'Riya',22,'Noida',92),
+(4,'Rahul',21,'Lucknow',67),
+(5,'Priya',20,'Rampur',88),
+(6,'Karan',23,'Delhi',74),
+(7,'Neha',22,'Jaipur',95),
+(8,'Ankit',21,'Noida',81),
+(9,'Simran',20,'Rampur',69),
+(10,'Rohit',22,'Delhi',90);
